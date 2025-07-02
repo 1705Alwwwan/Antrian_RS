77044911dfcb85f6a714fe73e6fb4850054a3033
Alwan Farhan Aziz <155723244+1705Alwwwan@users.noreply.github.com>
2025-07-02 21:34:25 +0700

    update_Abtruab)RS
    Latest verstion

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import QueueApp from "./QueueApp";
 import QueueList from "./QueueList";
+import { HashRouter as Router, Route, Routes } from "react-router-dom";
+
 
 function App() {
   return (
